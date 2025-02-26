5ded2002b483619bdc36fd34269dd992d2c01cc7
fnTodd <todd@function.network>
2025-02-26 12:14:05 -0700

    add command line options to run

---
+++ rime.py
@@ -0,0 +1,11 @@
+TALENT_MAP = {
+    1.1: "Chillbain",
+    1.2: "Coalescing Ice",
+    1.3: "Glacial Assault",
+    2.1: "Unrelenting Ice",
+    2.2: "Icy Flow",
+    2.3: "Tundra Guard",
+    3.1: "Avalanche",
+    3.2: "Wisdom of the North",
+    3.3: "Soulfrost Torrent",
+}
