c07b6dc646c453b9598710c9bc72c71ecbbb9ef5
Melioo <chosse.martiny01@gmail.com>
2025-03-01 19:32:37 +0100

    test: Next Spell wait
    Testing solution for Next Spell wait. Not functional.

--- Sim.py
+++ Sim.py
@@ -258,21 +258,19 @@ class Simulation:
             self.character.mana += 2
         self.character.mana += anima_gained
 
+        anima_spikes = self.character.spells["anima spikes"]
+
         for buff in self.buffs:
             if buff.name == "Ice Blitz":
                 for _ in range(int(anima_gained)):
-                    damage = self.character.spells["anima spikes"].damage(
-                        self.character
-                    )
+                    damage = anima_spikes.damage(self.character)
                     self.total_damage += damage
-                    self._fill_damage_table(
-                        self.character.spells["anima spikes"].name, damage
-                    )
+                    self._fill_damage_table(anima_spikes.name, damage)
 
                     if self.do_debug:
                         print(
                             f"Time {self.time:.2f}: "
-                            + f"Cast {self.character.spells['anima spikes'].name}, "
+                            + f"Cast {anima_spikes.name}, "
                             + f"dealing {damage:.2f} damage"
                         )
 
@@ -391,11 +389,66 @@ class Simulation:
                 None,
             )
 
+            if spell is not None:
+                check = False
+
+                # Check for spells
+                for test_spell in self.character.rotation:
+                    if spell.name != test_spell.name:
+                        print(
+                            spell.name,
+                            spell.effective_cast_time(self.character),
+                        )
+                        if (
+                            spell.effective_cast_time(self.character) == 0
+                            # and test_spell.remaining_cooldown > 0
+                            and test_spell.remaining_cooldown
+                            < (1.5 / (1 + self.character.haste / 100))
+                        ):
+                            print(
+                                f"Waiting for {test_spell.name} (GCD Trigger)"
+                            )
+                            self.update_time(test_spell.remaining_cooldown)
+                            check = True
+                            break
+
+                        if (
+                            test_spell.remaining_cooldown
+                            < spell.effective_cast_time(self.character)
+                            and test_spell.remaining_cooldown > 0
+                        ):
+                            print(f"Waiting for {test_spell.name}")
+                            self.update_time(test_spell.remaining_cooldown)
+                            check = True
+                            break
+
+                if check:
+                    continue
+
+            # if spell is not None and spell.name == "Frost Bolt":
+            #     nearest_ready_spell = min(
+            #         (
+            #             s
+            #             for s in self.character.rotation
+            #             if not s.is_ready(self.character, self.enemy_count)
+            #             and s.remaining_cooldown > 0
+            #         ),
+            #         key=lambda s: s.remaining_cooldown,
+            #     )
+            #     if (
+            #         nearest_ready_spell.remaining_cooldown
+            #         <= spell.effective_cast_time(self.character)
+            #     ):
+            #         self.update_time(nearest_ready_spell.remaining_cooldown)
+            #         continue
+
             if spell is None:
                 if self.do_debug:
                     print(f"Time {self.time:.2f}: No ready spell available")
                 self.update_time(0.1)
                 continue
+
+            print(f"Doing {spell.name}")
 
             self.gcd = 1.5 / (1 + self.character.haste / 100)
 
