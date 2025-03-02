c1dc9540753ca4e9786b091ee47bc6d3de3ff1ee
Toonic <Jord098@gmail.com>
2025-03-02 18:39:46 -0400

    Formatting + type final

--- base/spell.py
+++ base/spell.py
@@ -1,7 +1,9 @@
 """Module for the Spell class."""
+
 import random
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
+from typing import final
 
 if TYPE_CHECKING:
     from .character import BaseCharacter
@@ -13,14 +15,14 @@ class BaseSpell(ABC):
     def __init__(
         self,
         name="",
-        cast_time=0, #The casttime of the spell.
-        cooldown=0, #The cooldown of the spell.
-        damage_percent=0,  #The Damage the spell does based on the players main stat.
-        channeled=False, #If the spell is channeled or not.
-        ticks=1, #How many ticks of damage over the duration.
-        has_gcd=True, #If the spell has a GCD or not.
-        can_cast_on_gcd=False, #If the spell can be cast while GCD  is happening or not.
-        can_cast_while_casting=False, #If the spell can be cast while another spell is being cast.
+        cast_time=0,  # The casttime of the spell.
+        cooldown=0,  # The cooldown of the spell.
+        damage_percent=0,  # The Damage the spell does based on the players main stat.
+        channeled=False,  # If the spell is channeled or not.
+        ticks=1,  # How many ticks of damage over the duration.
+        has_gcd=True,  # If the spell has a GCD or not.
+        can_cast_on_gcd=False,  # If the spell can be cast while GCD  is happening or not.
+        can_cast_while_casting=False,  # If the spell can be cast while another spell is being cast.
     ):
         self.name = name
         self.cast_time = cast_time
@@ -37,36 +39,43 @@ class BaseSpell(ABC):
         """Returns the name of the spell in the simfell file."""
 
         return self.name.lower().replace(" ", "_")
-    
-    
+
     def is_ready(self, character: "BaseCharacter", enemy_count: int) -> bool:
         """Returns True if the spell is ready to be cast."""
 
     def effective_cast_time(self, character: "BaseCharacter") -> float:
         """Returns the effective cast time of the spell. Including any modifiers."""
 
+    @final
     def damage(self, character: "BaseCharacter") -> float:
         """Returns the damage of the spell. Including any modifiers."""
-        damage = self.damage_percent #The base damage of the spell.
-        damage = self.damage_modifiers(character, damage) #Any additional modifiers being applied.
-        damage = self.damage_modified_player_stats(character, damage) #The damage after being modified by player stats.
+        damage = self.damage_percent  # The base damage of the spell.
+        damage = self.damage_modifiers(
+            character, damage
+        )  # Any additional modifiers being applied.
+        damage = self.damage_modified_player_stats(
+            character, damage
+        )  # The damage after being modified by player stats.
 
-        #Roll for Crit Damage.
+        # Roll for Crit Damage.
         if random.uniform(0, 100) < self.get_crit_chance(character):
-            damage = damage * 2 #TODO: Include Crit Power.
+            damage = damage * 2  # TODO: Include Crit Power.
+
         return damage
-    
+
     # Used as an override for damage modifiers from Talents and other sources.
     def damage_modifiers(self, character: "BaseCharacter", damage) -> float:
         """Returns the damage of the spell. Including any modifiers."""
         return damage
-    
+
+    @final
     def damage_modified_player_stats(self, character: "BaseCharacter", damage) -> float:
         """Returns the damage of the spell after being modified by player stats"""
         modified_damage = damage * character.main_stat
         modified_damage = modified_damage * (1 + character.expertise / 100)
         return modified_damage
-    
+
+    @final
     def get_crit_chance(self, character: "BaseCharacter") -> float:
         """Returns the crit chance of the spell."""
         crit_chance = character.crit
@@ -89,22 +98,20 @@ class BaseSpell(ABC):
         """Decreases the remaining cooldown by the delta time."""
         self.remaining_cooldown -= delta_time
 
+
 class BaseDebuff(BaseSpell):
     """Abstract base class for all debuffs."""
+
     time_remaining = 0
 
-    def __init__(
-            self,
-            duration = 0, #The duration of the buff/debuff.
-            *args, **kwargs
-    ):
+    def __init__(self, duration=0, *args, **kwargs):  # The duration of the buff/debuff.
         super().__init__(*args, **kwargs)
         self.duration = duration
 
     def apply_debuff(self) -> None:
         """Applies the debuff to the target."""
         self.time_remaining = self.duration
-    
+
     def update_remaining_duration(self, delta_time: int) -> None:
         """Decreases the remaining buff/debuff duration by the delta time."""
         self.time_remaining -= delta_time
--- characters/Rime/rime.py
+++ characters/Rime/rime.py
@@ -1,8 +1,9 @@
 from base import BaseCharacter
 from base import BaseSpell
 
+
 class Rime(BaseCharacter):
-    """Stat Point DR """
+    """Stat Point DR"""
 
     main_stat_per_point = 1
     critPerPoint = 0.21
@@ -18,100 +19,106 @@ class Rime(BaseCharacter):
         self.anima = 0
         self.winter_orbs = 0
 
+
 class RimeSpell(BaseSpell):
     """Base information for Rime Spells"""
 
     anima_gained = 0
     winter_orb_cost = 0
 
-    def __init__(self,
-                 anima_gained = 0, 
-                 winter_orb_cost = 0,
-                 *args, **kwargs,):
+    def __init__(
+        self,
+        anima_gained=0,
+        winter_orb_cost=0,
+        *args,
+        **kwargs,
+    ):
         super().__init__(*args, **kwargs)
         self.anima_gained = anima_gained
         self.winter_orb_cost = winter_orb_cost
 
-    
 
 class Frost_Bolt(RimeSpell):
     """Frostbolt Spell"""
 
     def __init__(self):
-        super().__init__("Frostbolt",
-                         cast_time=1.5,
-                         damage_percent=73,
-                         anima_gained=3)
-        
+        super().__init__("Frostbolt", cast_time=1.5, damage_percent=73, anima_gained=3)
+
+
 class Cold_Snap(RimeSpell):
     """Cold Snap Spell"""
 
     def __init__(self):
-        super().__init__("Cold Snap",
-                         damage_percent=219,
-                         winter_orb_cost=-1)
-        
+        super().__init__("Cold Snap", damage_percent=219, winter_orb_cost=-1)
+
+
 class Freezing_Torrent(RimeSpell):
     """Freezing Torrent Spell"""
 
     def __init__(self):
-        super().__init__("Freezing Torrent",
-                         cast_time=2.0,
-                         cooldown=10,
-                         damage_percent=390,
-                         anima_gained=6,
-                         channeled=True,
-                         ticks=6)
-        
-#TODO: Make this a Debuff instead of a Spell.
+        super().__init__(
+            "Freezing Torrent",
+            cast_time=2.0,
+            cooldown=10,
+            damage_percent=390,
+            anima_gained=6,
+            channeled=True,
+            ticks=6,
+        )
+
+
+# TODO: Make this a Debuff instead of a Spell.
 class Bursting_Ice(RimeSpell):
     """Bursting Ice Spell"""
 
     def __init__(self):
-        super().__init__("Bursting Ice",
-                         cast_time=2.0,
-                         cooldown=15,
-                         damage_percent=366,
-                         anima_gained=6,
-                         ticks=6,
-                         is_debuff=True,
-                         duration=3)
-        
+        super().__init__(
+            "Bursting Ice",
+            cast_time=2.0,
+            cooldown=15,
+            damage_percent=366,
+            anima_gained=6,
+            ticks=6,
+            is_debuff=True,
+            duration=3,
+        )
+
+
 class Glacial_Blast(RimeSpell):
     """Glacial Blast Spell"""
 
     def __init__(self):
-        super().__init__("Glacial Blast",
-                         cast_time=2.0,
-                         damage_percent=504,
-                         winter_orb_cost=2)
-        
+        super().__init__(
+            "Glacial Blast", cast_time=2.0, damage_percent=504, winter_orb_cost=2
+        )
+
     def crit_chance_modifiers(self, character, crit_chance):
         if "Glacial Assault" in character.talents:
             crit_chance += 20
         return crit_chance
-        
+
+
 class Ice_Comet(RimeSpell):
     """Ice Comet Spell"""
 
     def __init__(self):
-        super().__init__("Ice Comet",
-                         damage_percent=300,
-                         winter_orb_cost=3)
-        
+        super().__init__("Ice Comet", damage_percent=300, winter_orb_cost=3)
+
+
 class Dance_Of_Swallows(RimeSpell):
     """Dance of Swallows Spell"""
 
     def __init__(self):
-        super().__init__("Dance of Swallows",
-                         cooldown=60,
-                         damage_percent=53,
-                         winter_orb_cost=2,)
+        super().__init__(
+            "Dance of Swallows",
+            cooldown=60,
+            damage_percent=53,
+            winter_orb_cost=2,
+        )
+
 
 class Ice_Blitz(RimeSpell):
     """Ice Blitz Spell"""
 
     def __init__(self):
-        super().__init__("Ice Blitz",
-                         cooldown=120,
-                         damage_percent=73)
+        super().__init__("Ice Blitz", cooldown=120, damage_percent=73)
