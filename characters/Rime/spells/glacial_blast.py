bf2c141a7b21106da83153584197e041aac24c5d
Toonic <Jord098@gmail.com>
2025-03-05 18:20:56 -0400

    Glacial Blast/Assault Combo

--- characters/Rime/spells/glacial_blast.py
+++ characters/Rime/spells/glacial_blast.py
@@ -2,6 +2,7 @@
 
 from characters.rime import RimeSpell
 from characters.rime.talent import RimeTalents
+from characters.rime.buffs import GlacialAssault
 
 
 class GlacialBlast(RimeSpell):
@@ -15,7 +16,21 @@ class GlacialBlast(RimeSpell):
             winter_orb_cost=2,
         )
 
+    def is_ready(self):
+        glacial_assault = GlacialAssault()
+        if (
+            RimeTalents.GLACIAL_ASSAULT in self.character.talents
+            and glacial_assault.simfell_name in self.character.buffs
+        ):
+            if (
+                self.character.buffs[glacial_assault].current_stacks
+                == self.character.buffs[glacial_assault].max_stacks
+            ):
+                self.cast_time = 0
+            else:
+                self.cast_time = 2  # I don't like this.
+
     def crit_chance_modifiers(self, crit_chance):
         if RimeTalents.GLACIAL_ASSAULT in self.character.talents:
-            crit_chance += 20
+            crit_chance += 20  # I also don't like this.
         return crit_chance
