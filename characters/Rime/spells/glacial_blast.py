a3fe0888863b299cf965160c3ca75c0e8a30f5c4
Toonic <Jord098@gmail.com>
2025-03-06 08:48:48 -0400

    Blast and prep for new talents.

--- characters/Rime/spells/glacial_blast.py
+++ characters/Rime/spells/glacial_blast.py
@@ -8,6 +8,8 @@ from characters.rime.buffs import GlacialAssault
 class GlacialBlast(RimeSpell):
     """Glacial Blast Spell"""
 
+    glacial_assault = None
+
     def __init__(self):
         super().__init__(
             "Glacial Blast",
@@ -17,24 +19,38 @@ class GlacialBlast(RimeSpell):
         )
 
     def effective_cast_time(self):
-        """Overrides the effective cast time if you're at max stacks."""
-        if RimeTalents.GLACIAL_ASSAULT in self.character.talents:
-            glacial_assault = GlacialAssault().simfell_name
-            if glacial_assault in self.character.buffs:
-                if (
-                    self.character.buffs[glacial_assault].current_stacks
-                    == self.character.buffs[glacial_assault].maximum_stacks
-                ):
-                    return 0
-
+        """Overrides the effective cast time if you have maximum stacks of glacial assault."""
+        if self.is_glacial_assault_ready():
+            return 0
         return super().effective_cast_time()
+
+    def damage_modifiers(self, damage) -> float:
+        if self.is_glacial_assault_ready():
+            return damage * 2
+        else:
+            return damage
+
+    def on_cast_complete(self):
+        super().on_cast_complete()
+        if self.is_glacial_assault_ready():
+            self.character.buffs[self.glacial_assault].remove_buff()
 
     def crit_chance_modifiers(self, crit_chance):
         if RimeTalents.GLACIAL_ASSAULT in self.character.talents:
-            # Mel's NOTE: Maybe you can create a class attribute that would
-            # be used to determine additional modifiers.
-            # E.g. self.additional_modifiers: Dict[str, float] = {}
-            # crit_chance += self.additional_modifiers.get("crit_chance", 0)
-
-            crit_chance += 20  # I also don't like this.
+            crit_chance += (
+                20  # TODO: Move the 20 to a Glacial_Assault talent class.
+            )
         return crit_chance
+
+    def is_glacial_assault_ready(self) -> bool:
+        if RimeTalents.GLACIAL_ASSAULT in self.character.talents:
+            self.glacial_assault = GlacialAssault().simfell_name
+            if self.glacial_assault in self.character.buffs:
+                if (
+                    self.character.buffs[self.glacial_assault].current_stacks
+                    == self.character.buffs[
+                        self.glacial_assault
+                    ].maximum_stacks
+                ):
+                    return True
+        return False
